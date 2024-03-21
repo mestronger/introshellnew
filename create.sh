730ba209166b9b9fa24af3a00df9389426202560
I must be strong <164299950+mestronger@users.noreply.github.com>
2024-03-21 12:26:26 +0700

    Update create.sh

--- create.sh
+++ create.sh
@@ -14,7 +14,7 @@ echo "
 ██║██║░╚███║░░░██║░░░██║░░██║╚█████╔╝
 ╚═╝╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░"
 
-echo "=> Nama: rafl ananda putra"
+echo "=> Nama: rafly ananda putra"
 echo "=> Usia: 19 tahun"
 echo "=> Kelas: 12 TKR"
 echo "=> Adress: pku, city - indonesia"
